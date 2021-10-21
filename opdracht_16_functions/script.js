89f3ed59251bf819098e905766b57e7cfa9a2a19
DashaDafusun <84176953+DashaDafusun@users.noreply.github.com>
2021-10-21 13:19:20 +0200

    Add files via upload

---
+++ opdracht_16_functions/script.js
@@ -0,0 +1,28 @@
+
+
+let makePizza = function () {
+    console.log('Take a dough');
+    console.log('Brush the rim of the pizza with olive oil');
+    console.log('Spread the pizza sauce evenly onto the pizza');
+    console.log('Sprinkle the herbs onto the pizza and top with the cheese');
+    console.log('Slide the pizza onto the tile and bake for 7 minutes');
+    console.log('Rest for 3 minutes before slicing');
+};
+
+let makeSuchi = function () {
+    console.log('1.Cook a batch of sushi rice in your rice cooker');
+    console.log('2.Heat your vinegar, sugar, and salt together in a small saucepan');
+    console.log('3.Cut your filling ingredients into small pieces');
+    console.log('4.Place a sheet of nori on your rolling mat with the shiny side down');
+    console.log('5.Spread a thin layer of sushi rice onto the sheet of nori');
+    console.log('6.Add roughly 2 ounces (57 g) of filling ingredients to the rice');
+    console.log('7.Roll the sushi up from the bottom using your mat as a guide');
+    console.log('8.Cut your roll into 1–2 in (2.5–5.1 cm) pieces');
+};
+
+
+makeSuchi();
+makeSuchi();
+makeSuchi();
+makePizza();
+makePizza();
